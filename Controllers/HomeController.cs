552992f1f991699b6e94cce462253b5f6f66407b
charendra910 <charendra910@rku.ac.in>
2025-02-28 18:45:59 +0530

    product items are added to frontend of the web application

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,29 @@
-using System.Diagnostics;
+using E_CommerceMVC.Data;
 using E_CommerceMVC.Models;
+using E_CommerceMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace E_CommerceMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _context.Products.ToListAsync();
+            var model = new HomeViewModel { Products = products };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using E_CommerceMVC.Models;
+
+namespace E_CommerceMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Product> Products { get; set; }
+
+    }
+}
